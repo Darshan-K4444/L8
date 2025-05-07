e2764d5b1f6d19c49892b32672c40248e40857da
Darshan-K4444 <darshan.k@s.amity.edu>
2025-05-07 12:35:57 +0530

    adding the numbers

---
+++ p.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    double num1, num2, sum;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+
+    sum = num1 + num2;
+
+    std::cout << "The sum is: " << sum << std::endl;
+
+    return 0;
+}
